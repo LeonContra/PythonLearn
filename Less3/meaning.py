59a417f1f7ae5358be17ca0bff58e8c0a3892422
Leo <leonardoc92001@gmail.com>
2024-09-03 12:19:06 -0700

    Added Lesson 3 and descriptions of lessons

---
+++ Less3/meaning.py
@@ -0,0 +1,10 @@
+meaning = 35
+print ('')
+
+# if meaning > 10:
+#     print('Yeah! That is correct')
+# else:
+#     print("Uhh, Nope.")
+
+# Ternary Operator
+print('Yeah! That is correct') if meaning > 10 else print('Uhh, Nope.')
