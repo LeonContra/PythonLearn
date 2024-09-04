a2071d9bd1e92fdf6a00f30e0a27b90aebaacbca
Leo <leonardoc92001@gmail.com>
2024-09-04 10:08:29 -0700

    Added Lesson 5

---
+++ Less05/rps.py
@@ -0,0 +1,45 @@
+import sys
+import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+ 
+# print(RPS(2))
+# print(RPS.ROCK)
+# print(RPS['ROCK'])
+# print(RPS.ROCK.value)
+# sys.exit() 
+
+acceptedInput = {'1', '2', '3'}
+
+print("")
+playerChoice = input("Enter...\n1 for rock, \n2 for paper or \n3 for Scissors:\n\n")
+
+player = int(playerChoice) if playerChoice in acceptedInput else sys.exit("Input must be a number")
+    
+
+if player < 1 | player > 3: 
+    sys.exit("You must enter 1, 2, or 3")
+    
+computerChoice = random.choice("123")
+
+computer = int(computerChoice)
+
+print("")
+print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+print("")
+
+if player ==  1 and computer == 3:
+    print("🎉 You win!")
+elif player ==  2 and computer == 1:
+    print("🎉 You win!")
+elif player ==  3 and computer == 2:
+    print("🎉 You win!")
+elif player == computer:
+    print("😑 You tied.")
+else:
+    print("👺 Python wins...")
