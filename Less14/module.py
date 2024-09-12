de55e91f480ec87a8700134ee7cbbbd7f733c7bd
Leo <leonardoc92001@gmail.com>
2024-09-12 08:44:39 -0700

    Modified RPS to become a module

--- Less14/module.py
+++ Less14/module.py
@@ -3,6 +3,7 @@ import sys
 import random as rdm
 from enum import Enum
 import cali
+from rps7 import rock_paper_scissors
 
 
 print(pi)
@@ -15,3 +16,5 @@ cali.randomfunfact()
 
 print(__name__)
 print(cali.__name__)
+
+rock_paper_scissors()
